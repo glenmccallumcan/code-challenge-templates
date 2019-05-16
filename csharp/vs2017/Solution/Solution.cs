f3e8fbe273b6d02c2df7da159286a207fcf7d9c5
Glen McCallum <glen.mccallum@protonmail.com>
2019-05-16 08:30:36 -0700

    Initial Commit
    - template for c# code challenges using visual
    studio 2017
    - prepopulated with dictionary data structure since
    they take the most work to code up in c#. easier to
    convert this to array data structure than to go the
    other way

---
+++ csharp/vs2017/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public static class Solution
+    {
+        public static List<int> SolutionMethod(List<List<int>> queries)
+        {
+            return new List<int>();
+        }
+
+        private static Dictionary<int, int> EnsureExists(
+        this Dictionary<int, int> dict,
+        int value)
+        {
+            if (!dict.ContainsKey(value))
+            {
+                dict.Add(value, 0);
+            }
+
+            return dict;
+        }
+    }
+}
